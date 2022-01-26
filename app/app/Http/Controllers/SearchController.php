783f724c56c20718f4ec090044666adef8e55d84
rickeysan <rickeysan95@gmail.com>
2022-01-27 08:54:02 +0900

    キーワードによる検索はできた

--- app/app/Http/Controllers/SearchController.php
+++ app/app/Http/Controllers/SearchController.php
@@ -14,4 +14,27 @@ class SearchController extends Controller
         // dd($habits);
         return view('post/search',compact('habits','total_habits_amount'));
     }
+
+    public function store(Request $request){
+        logger('PostControllerのstoreメソッドです');
+        logger($request);
+        $key_word = $request->input('key_word');
+        //検索キーワードが空の場合
+        if (empty($key_word)) {
+            $users = Habit::paginate(10);  
+        //検索キーワードが入っている場合
+        } else {
+            // $_key_word = str_replace('　', ' ', $key_word);  //全角スペースを半角に変換
+            // $_key_word = preg_replace('/\s(?=\s)/', '', $_key_word); //連続する半角スペースは削除
+            // $_key_word = trim($_key_word); //文字列の先頭と末尾にあるホワイトスペースを削除
+            // $_key_word = str_replace(['\\', '%', '_'], ['\\\\', '\%', '\_'], $_key_word); //円マーク、パーセント、アンダーバーはエスケープ処理
+            // $keywords = array_unique(explode(' ', $_key_word)); //キーワードを半角スペースで配列に変換し、重複する値を削除
+
+            $habits = Habit::where('task','like','%'.$key_word.'%')->paginate(10);   
+            $total_habits_amount = count(Habit::where('task','like','%'.$key_word.'%')->get());
+        }
+        return view('post/search',compact('habits','total_habits_amount'));
+    }
+
+
 }
--- app/resources/views/post/search.blade.php
+++ app/resources/views/post/search.blade.php
@@ -19,9 +19,21 @@
 </head>
 <body>
     <h1>習慣を探す</h1>
+    <form action=" {{ route('search.store') }}" method="post">
+        @csrf
+        <label>キーワード</label>
+        <input type="text" name="key_word">
+        <label>カテゴリ</label>
+        <select name="category_id" id="">
+            <option value="1">No.1</option>
+            <option value="2">No.2</option>
+            <option value="3">No.3</option>
+        </select>
+        <button tupe="submit">検索する</button>
+    </form>
     <div>
         <p>{{ $total_habits_amount }}件の習慣が見つかりました</p>
-        
+
         <div class="habit-card-wrap">
 
             @foreach ($habits as $habit)
