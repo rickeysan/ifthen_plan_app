9607b7b2954877aa31c1e49845f527a8f437da4b
rickeysan <rickeysan95@gmail.com>
2022-01-28 14:18:43 +0900

    習慣の閲覧画面を作成

--- app/resources/views/habit/show.blade.php
+++ app/resources/views/habit/show.blade.php
@@ -79,7 +79,7 @@
                                     <p class="form-item__head-text">なんのために習慣化をしますか？</p>
                                 </div>
                                 <div class="form-item__body">
-                                    <textarea class="form-item__textarea" name="purpose">{{ $habit['purpose'] }}</textarea>
+                                    <textarea class="form-item__textarea" name="purpose" readonly>{{ $habit['purpose'] }}</textarea>
                                 </div>
                             </div>
                             <div class="form-item">
@@ -87,7 +87,7 @@
                                     <p class="form-item__head-text">目的達成のために何をしますか？</p>
                                 </div>
                                 <div class="form-item__body">
-                                    <textarea class="form-item__textarea" name="tast">{{ $habit['task'] }}</textarea>
+                                    <textarea class="form-item__textarea" name="tast" readonly>{{ $habit['task'] }}</textarea>
                                 </div>
                             </div>
 
@@ -96,20 +96,17 @@
                 <section class="calendar-section">
                     <div  class="calendar" id='calendar'></div>
                     <div class="calender-info__container">
-                        <p class="calendar-info-title">記録の追加・編集フォーム</p>
+                        <p class="calendar-info-title">記録フォーム</p>
                         <form action="" class="calendar-input-form" style="display: none;">
-                            <p class="calendar-input-title"></p>
+                            <p class="calendar-input-title" style="display:none"></p>
                             <p><input type="date" name="start_date" class="calendar-input-date" readonly></p>
-                            <textarea class="calendar-input__textarea js-textarea" type="text" name="title" class="calendar-input-text" placeholder="上手くいったことや反省点を記入"></textarea>
+                            <textarea class="calendar-input__textarea js-textarea" type="text" name="title" class="calendar-input-text" placeholder="記録がありません" readonly></textarea>
                             <div class="calendar-input__radio-wrap">
-                                <input class="calendaer-input__radio" type="radio" id="rd0" name="achivement_flg" value="0">達成
-                                <input class="calendaer-input__radio" type="radio" id="rd1" name="achivement_flg" value="1">例外日
+                                <input class="calendaer-input__radio" type="radio" id="rd0" name="achivement_flg" value="0" readonly>達成
+                                <input class="calendaer-input__radio" type="radio" id="rd1" name="achivement_flg" value="1" readonly>例外日
                             </div>
                         </form>
-                        <div class="calendar-input__btn-wrap">
-                            <button class="calendar-input__btn" id="btn-store" style="display: none;">登録</button>
-                            <button class="calendar-input__btn" id="btn-edit" style="display: none;">編集</button>
-                        </div>
+
                     </div>
                 </section>
             </div>
