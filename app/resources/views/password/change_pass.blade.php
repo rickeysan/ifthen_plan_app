9d7ad233d27b2877ac903e0acfe5005eeb323180
rickeysan <rickeysan95@gmail.com>
2022-01-28 21:01:29 +0900

    パスワード変更画面、プロフィール編集画面のデザインを移植完了

--- app/resources/views/password/change_pass.blade.php
+++ app/resources/views/password/change_pass.blade.php
@@ -4,31 +4,93 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>パスワード変更</h1>
-    <form action="{{ route('change.store') }}" method="post">
-        @csrf
-        @error('current_pass')
-            <p>{{$message}}</p>
-        @enderror
-        <label>現在のパスワード</label>
-        <input type="text" name="current_pass" value="{{ old('current_pass') }}">
+    <header id="header">
+        <div class="inner">
+            <h1 class="logo">
+                <a href=""><img src="img/top_banner.png" alt=""></a>
+            </h1>
+            <nav>
+                <ul class="header__nav-list">
+                    <li class="logined-header__nav-item">
+                        <a href="">
+                        <img class="logined-header__nav-item__img" src="img/top_banner.png" alt="">
+                        </a>
+                    </li>
+                    <li><a href="">マイページ</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main id="main">
+        <div class="main__inner columns-2">
+            <nav class="dashbord-menu__list-nav">
+                <ul class="dashbord-menu__list-ul">
+                    <li class="dashbord-menu__list-item"><a class="dashbord-menu__list-item-link" href=""><i class="fas fa-home"></i>HOME</a></li>
+                    <li class="dashbord-menu__list-item"><a class="dashbord-menu__list-item-link" href=""><i class="far fa-calendar-plus"></i>新しい習慣</a></li>
+                    <li class="dashbord-menu__list-item"><a class="dashbord-menu__list-item-link" href=""><i class="fas fa-search"></i>習慣を検索</a></li>
+                    <li class="dashbord-menu__list-item"><a class="dashbord-menu__list-item-link" href=""><i class="far fa-id-card"></i>プロフィール</a></li>
+                    <li class="dashbord-menu__list-item"><a class="dashbord-menu__list-item-link" href=""><i class="far fa-heart"></i>フォローリスト</a></li>
+                </ul>
+            </nav>
 
-        @error('new_pass')
-            <p>{{$message}}</p>
-        @enderror
-        <label>新しいパスワード</label>
-        <input type="text" name="new_pass">
+            <section class="profile-edit__section">
+                <h2 class="section-title">パスワード変更</h2>
+                <div class="profile-edit__container">
+                    <form action="" method="post">
+                        @csrf
+                        <div class="form-item">
+                            <div class="form-item__info-wrap">
+                                <span class="form-item__title">現在のパスワード</span>
+                            </div>
+                            <div class="form-item__input-area">
+                                <input type="text" name="current_pass" value="{{ old('current_pass') }}" class="form-item__email form-item__input">
+                                @error('current_pass')
+                                    <span class="form-item__input-area__msg err-msg">{{ $message }}</span>
+                                @enderror
+                            </div>
 
-        @error('new_pass_confirmation')
-            <p>{{$message}}</p>
-        @enderror
-        <label>新しいパスワード（再入力）</label>
-        <input type="text" name="new_pass_confirmation">
+                        </div>
+                        <div class="form-item">
+                            <div class="form-item__info-wrap">
+                                <span class="form-item__title">新しいパスワード</span>
+                            </div>
+                            <div class="form-item__input-area">
+                                <input type="password" name="new_pass" class="form-item__name form-item__input">
+                                @error('new_pass')
+                                    <span class="form-item__input-area__msg err-msg">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
 
-        <input type="submit" vame="変更する">
-    </form>
+                        <div class="form-item">
+                            <div class="form-item__info-wrap">
+                                <span class="form-item__title">新しいパスワード<br>（確認用)</span>
+                            </div>
+                            <div class="form-item__input-area">
+                                <input type="text" name="name" class="form-item__name form-item__input">
+                                @error('new_pass_confirmation')
+                                    <span class="form-item__input-area__msg err-msg">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-item__btn-wrap">
+                            <button class="form-item__btn">保存する</button>
+                        </div>
+
+                    </form>
+                </div>
+            </section>
+        </div>
+    </main>
+
+
 </body>
 </html>
+
--- app/resources/views/profile.blade.php
+++ app/resources/views/profile.blade.php
@@ -4,66 +4,111 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <title>Document</title>
-    <style>
-        .change-image-wrap {
-        position: relative;
-        border: 3px dotted black;
-        line-height: 150px;
-        text-align: center;
-        width: 150px;
-        height: 150px;}
-        .input-image{
-            opacity: 0;
-        }
-        .drop-image{
-            opacity: 0;
-        }
-  .input-image,
-  .drop-image,
-  .show-db-image {
-    position: absolute;
-    display: block;
-    width: 100%;
-    height: 100%;
-  }
-  .input-image {
-    z-index: 3;
-  }
-  .drop-image {
-    z-index: 1;
-  }
-  .show-db-image {
-    z-index: 0;
-  }
-}
-    </style>
 </head>
+
 <body>
-    <h1>プロフィール編集</h1>
-    <div class="main-contents-wrap">
-        <form action="{{ route('profile.store') }}" method="post" enctype="multipart/form-data">
-            @csrf
-            <label>名前</label>必須
-            <input type="text" name="name" value="{{  $user_info[0]['name'] }}">
-            <label>メールアドレス</label>必須
-            <input type="text" name="email" value="{{  $user_info[0]['email'] }}">
-            <label>プロフィール画像</label>任意
-            <div class="change-image-wrap">
-                <input type="file" name="image" accept="image/png, image/jpeg" class="input-image js-droparea">
-                <img src="" class="drop-image js-show-image" alt="jsから来た画像">
-                @if(!empty($user_info[0]['file_path']))
-                    <img class="show-db-image" src="{{ asset( Storage::url($user_info[0]['file_path'])) }}" alt="プロフィール画像">
-                @endif
-                ドラッグ＆ドロップ
-            </div>
-            <label>自己紹介</label>任意
-            <textarea name="introduction" cols="80" rows="5">{{ $user_info[0]['introduction'] }}</textarea>
-            <input type="submit" value="変更する">
-        </form>
-    </div>
-    <a href="/logout">ログアウトする</a>
-    <a href="/password/change">パスワードを変更する</a>
+    <header id="header">
+        <div class="inner">
+            <h1 class="logo">
+                <a href=""><img src="/app/public/img/top_banner.png" alt=""></a>
+            </h1>
+            <nav>
+                <ul class="header__nav-list">
+                    <li class="logined-header__nav-item">
+                        <a href="">
+                        <img class="logined-header__nav-item__img" src="/app/public/img/top_banner.png" alt="">
+                        </a>
+                    </li>
+                    <li><a href="">マイページ</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main id="main">
+        <div class="main__inner columns-2">
+            <nav class="dashbord-menu__list-nav">
+                <ul class="dashbord-menu__list-ul">
+                    <li class="dashbord-menu__list-item"><a class="dashbord-menu__list-item-link" href=""><i class="fas fa-home"></i>HOME</a></li>
+                    <li class="dashbord-menu__list-item"><a class="dashbord-menu__list-item-link" href=""><i class="far fa-calendar-plus"></i>新しい習慣</a></li>
+                    <li class="dashbord-menu__list-item"><a class="dashbord-menu__list-item-link" href=""><i class="fas fa-search"></i>習慣を検索</a></li>
+                    <li class="dashbord-menu__list-item"><a class="dashbord-menu__list-item-link" href=""><i class="far fa-id-card"></i>プロフィール</a></li>
+                    <li class="dashbord-menu__list-item"><a class="dashbord-menu__list-item-link" href=""><i class="far fa-heart"></i>フォローリスト</a></li>
+                </ul>
+            </nav>
+
+            <section class="profile-edit__section">
+                <h2 class="section-title">プロフィール編集</h2>
+                <div class="profile-edit__container">
+                    <form action="{{ route('profile.store') }}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-item">
+                            <div class="form-item__info-wrap">
+                                <span class="form-item__title">メールアドレス</span><span class="form-item__title__batch batch-must">必須</span>
+                            </div>
+                            <div class="form-item__input-area">
+                                <input type="text" name="email" value="{{  $user_info[0]['email'] }}" class="form-item__email form-item__input">
+                                @error('email')
+                                    <span class="form-item__input-area__msg err-msg">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-item">
+                            <div class="form-item__info-wrap">
+                                <span class="form-item__title">ユーザー名</span><span class="form-item__title__batch batch-must">必須</span>
+                            </div>
+                            <div class="form-item__input-area">
+                                <input type="text" name="name" value="{{  $user_info[0]['name'] }}" class="form-item__name form-item__input">
+                                @error('name')
+                                    <span class="form-item__input-area__msg err-msg">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-item__image">
+                            <div class="form-item__info-wrap">
+                                <span class="form-item__title">アイコン画像</span><span class="form-item__title__batch batch-option">任意</span>
+                            </div>
+                            <div class="form-item__image-container">
+                                <div class="form-item__image-wrap">
+                                    <input type="file" name="image" accept="image/png, image/jpeg" class="input-image js-droparea">
+                                    <img src="" class="drop-image js-show-image" alt="jsから来た画像">
+                                    @if(!empty($user_info[0]['file_path']))
+                                        <img class="show-db-image" src="{{ asset( Storage::url($user_info[0]['file_path'])) }}" alt="プロフィール画像">
+                                    @endif
+                                    ドラッグ＆ドロップ
+                                </div>
+                                @error('image')
+                                    <span class="form-item__input-area__msg-image err-msg">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+                        </div>
+                        <div class="form-item__textarea">
+                            <div class="form-item__info-wrap">
+                                <span class="form-item__title">自己紹介</span><span class="form-item__title__batch batch-option">任意</span>
+                            </div>
+                            <div class="form-item__input-area">
+                                <textarea name="introduction" cols="80" rows="5" class="form-item__textarea__input">{{ $user_info[0]['introduction'] }}</textarea>
+                                @error('introduction')
+                                    <span class="form-item__input-area__msg err-msg">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-item__btn-wrap">
+                            <button class="form-item__btn">保存する</button>
+                        </div>
+
+                    </form>
+                </div>
+            </section>
+        </div>
+    </main>
 </body>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script>
